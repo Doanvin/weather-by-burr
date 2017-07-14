7968af9cc76f1aba674d38fd8876b41c59abbcd2
Donavin <donavinhannon@gmail.com>
2017-07-14 16:33:38 -0700

    Added event listener to search__input, clean scss, added async or defer to scripts

--- src/assets/js/weatherApi.js
+++ src/assets/js/weatherApi.js
@@ -1,24 +1,34 @@
+(document.getElementsByClassName('search__input')[0]
+    .addEventListener('keyup', (event) => {
+        event.preventDefault();
+        if (event.keyCode == 13) {
+            document.getElementsByClassName('search__button')[0].click();
+        }
+    })
+());
+
 /* exported getWeatherItem */
 function getWeatherItem() {
-    let queryText = document.getElementsByClassName('weather__query-item--start')[0].value;
+    let queryText = document.getElementsByClassName('search__input')[0].value;
     queryText = queryText.replace(/,/, '%2C').replace(/ /, '%20');
     const startUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22';
     const endUrl = '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=parseWeatherItem';
-  // Format the url
+    // Format the API call url
     let url = startUrl + queryText + endUrl;
     url = url.replace(/"/, '');
-  // Create the script element that makes the api call and uses
-  // parseWeatherItem() as a callback.
+    // Create the script element that makes the api call and uses
+    // parseWeatherItem() as a callback.
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = url;
+    script.setAttribute('async','');
     document.body.appendChild(script);
 }
 
 /* exported parseWeatherItem */
 function parseWeatherItem (o) {
-  // Parses returned response, o, and extracts
-  // the title, links, and text of each news story.
+    // Parses returned response, o, and extracts
+    // the title, links, and text of each news story.
     const results = o.query.results.channel;
     const city = results.location.city;
     const state = results.location.region;
@@ -30,8 +40,8 @@ function parseWeatherItem (o) {
 }
 
 function createWeatherCurrent (city, state, country, currentConditions) {
-  // Takes parsed weather info and creates a div to be placed in the
-  // #weatherCurrent div. All variables passed in should be strings.
+    // Takes parsed weather info and creates a div to be placed in the
+    // .weatherCurrent div. All variables passed in should be strings.
     let location = document.getElementsByClassName('weather__location')[0];
     location.innerHTML = city + ', ' + state + '<br/>' + country;
 
@@ -40,6 +50,8 @@ function createWeatherCurrent (city, state, country, currentConditions) {
 }
 
 function createWeatherForecast (forecasts) {
+    // Takes parsed weather info and creates a div to be placed in the
+    // .weather__forecast div. Forecasts is an array of objects.
     const noForecasts = forecasts.length;
     let forecastHtml = '';
     for (let i = 0; i < noForecasts; i++) {
@@ -47,7 +59,7 @@ function createWeatherForecast (forecasts) {
         const high = forecasts[i].high;
         const low = forecasts[i].low;
         const text = forecasts[i].text;
-        forecastHtml += '<div class="weather__forecast-item col-md-2"><h3>' + day + '</h3><p>' + high + '</p><p>' + low + '</p><p>' + text + '</p></div>';
+        forecastHtml += '<div class="weather__forecast-item col-xs-6 col-sm-3 col-md-2"><h3>' + day + '</h3><p>' + high + '</p><p>' + low + '</p><p>' + text + '</p></div>';
     }
     // Remove hidden class to display weather__forecast-title div
     const forecastTitle = document.getElementsByClassName('weather__forecast-title')[0];
